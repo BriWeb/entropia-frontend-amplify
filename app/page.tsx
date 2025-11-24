88e19d91f1f8a3cd544437ef18c14dcf096bb288
Iara Evelyn Baya Vargas <iarabayav@gmail.com>
2025-11-24 00:13:38 -0300

    feat: agrego imagenes beneficios de la app

--- app/page.tsx
+++ app/page.tsx
@@ -14,31 +14,31 @@ export default function Home() {
       quote:
         "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
       name: "Comodidad 24 x 7",
-      src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "/images/foto1.jpg",
     },
     {
       quote:
         "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
       name: "Reduccion de esperas",
-      src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "/images/foto2.jpg",
     },
     {
       quote:
         "This solution has significantly improved our team's productivity. ",
       name: "Recordatorios automaticos",
-      src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "/images/foto3.jpg",
     },
     {
       quote:
         "Outstanding support and robust features. It's rare to find a product that delivers on all its promises.",
       name: "Optimizacion de flujo de trabajo",
-      src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "/images/foto4.jpg",
     },
     {
       quote:
         "The scalability and performance have been game-changing for our organization. Highly recommend to any growing business.",
       name: "Mejor gestion del tiempo médico",
-      src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "/images/medicos.png",
     },
   ];
 
@@ -182,7 +182,7 @@ export default function Home() {
                 <a
                   key={item.name}
                   href={item.href}
-                  className="text-gray-600 dark:text-white hover:text-blue-600 transition duration-150 font-medium px-3 py-2 rounded-md"
+                  className="text-gray-600 dark:text-white hover:text-blue-600 dark:hover:bg-gray-500 transition duration-150 font-medium px-3 py-2 rounded-md"
                   onClick={(e) => {
                     e.preventDefault();
                     const element = document.getElementById(
@@ -320,7 +320,7 @@ export default function Home() {
               {membersItems.map((member, index) => (
                 <Card
                   key={index}
-                  className="w-[250px] border border-neutral-200 dark:border-primery bg-transparent hover:bg-neutral-50 dark:hover:bg-primary transition-all duration-300 shadow-[0_0_10px] shadow-white-400/30"
+                  className="w-[250px] border border-neutral-200 dark:border-primery bg-transparent hover:bg-neutral-50 dark:hover:bg-primary/30 transition-all duration-300 shadow-[0_0_10px] shadow-white-400/30"
                 >
                   <CardContent className="p-6 flex flex-col items-center h-full">
                     <Image
